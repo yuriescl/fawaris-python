49299d23396db6f11a8e760d0b36076e7ce92182
Yuri <yuriescl@gmail.com>
2022-03-17 23:52:24 -0300

    wip

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 poetry env use python3.7
 poetry install
 poetry shell
+python -m unittest discover tests
 ```
--- tests/test_sep10.py
+++ tests/test_sep10.py
@@ -7,31 +7,41 @@ from fawaris.sep10 import Sep10, Sep10GetRequest, Sep10PostRequest
 class TestSep10(unittest.TestCase):
     def test_sep10(self):
         async def _async():
+            server_signing_secret = (
+                "SALAW2MR7I7L47W5ZKJXJMGUZSKRSTVUQ7O3ZHEZ6O7MUKLDGWZ5U2JW"
+            )
+            client_account_secret = (
+                "SBLZPFTQY74COGBRJYKC6Y3X46KDYO46BPBRZK27IXUQ73DP6IDNUB7X"
+            )
+
             sep10 = Sep10(
                 "http://localhost",
                 ["localhost"],
                 "https://horizon-testnet.stellar.org",
                 Network.TESTNET_NETWORK_PASSPHRASE,
-                "SD3ME2YQNWQYBKYX7KNMX5C42WTWMZRZD7DH72K63B56G636AYBQH7YY",
+                server_signing_secret,
                 "jwtsecret",
             )
+
+            client_kp = Keypair.from_secret(client_account_secret)
             resp = await sep10.get(
                 Sep10GetRequest(
-                    account="GACGKVS4ECC2UUECHQGWPO5YY66T7FDPTEBTDD4X445APZPWWT5YPRMP",
+                    account=client_kp.public_key,
                     home_domain="localhost",
                 )
             )
+
             envelope_xdr = resp.transaction
             envelope = TransactionEnvelope.from_xdr(
                 envelope_xdr, Network.TESTNET_NETWORK_PASSPHRASE
             )
-            client_signing_key = Keypair.from_secret(
-                "SAXTOCC6EYFF7EJQJETNRTUQTM4PGYXWO3O4BCNDGEQ3EJHNX4MBY2Y6"
-            )
+            client_signing_key = Keypair.from_secret(client_account_secret)
             envelope.sign(client_signing_key)
             client_signed_envelope_xdr = envelope.to_xdr_object().to_xdr()
-            request = Sep10PostRequest(transaction=client_signed_envelope_xdr)
-            resp = await sep10.post(request)
+
+            resp = await sep10.post(
+                Sep10PostRequest(transaction=client_signed_envelope_xdr)
+            )
             assert resp.token
 
         asyncio.run(_async())
